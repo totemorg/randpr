d61ad78bcf05c97482f04a660ed8f22df0268d39
acmesds <acmesds@email.com>
2017-04-04 17:15:52 -0500

    refactor interface for flex engine

--- randpr.js
+++ randpr.js
@@ -1,13 +1,11 @@
 var 														// Totem modules
-	ENUM = require("../enum"),
-	MVN = require("multivariate-normal").default,
-	EM = require("expectation-maximization");
+	ENUM = require("../enum");
 	
 var															// shortcuts
 	Copy = ENUM.copy,
 	Each = ENUM.each;
 
-var RP = module.exports = {
+var RAN = module.exports = {
 	N: 0, 		// ensemble size
 	K: 0, 		// #states
 	E: null,    // [N] ensemble 
@@ -36,14 +34,23 @@ var RP = module.exports = {
 	steps: 0, // step counter	
 	jumps: 0, // number of jumps
 	reversible: false,
-	x: null, // observations
+	x: null, // jump observations
+	y: null, // step observations
+
+	MVN: require("multivariate-normal").default,
+	MLE: require("expectation-maximization"),
 	
-	jump: function (fr) {  
+	cb: {
+		jump: function () {},
+		step: function () {}
+	},
+
+	jump: function (fr, cb) {  
 		
-		var K = RP.K, R = RP.R, P = RP.P, A = RP.A;
+		var K = RAN.K, R = RAN.R, P = RAN.P, A = RAN.A;
 
 		function Poisson( fr ) {
-			for (var to=0,Pfr=P[fr],Afr=A[fr],dt=RP.dt; to<K; to++) 
+			for (var to=0,Pfr=P[fr],Afr=A[fr],dt=RAN.dt; to<K; to++) 
 				Pfr[to] =  (to == fr) ? 0 : Afr[to]*dt;  // disallows fr-fr jump
 		
 			//console.log(["pb",fr,Pfr]);
@@ -62,7 +69,7 @@ var RP = module.exports = {
 			//console.log(["ga",fr,Pfr]);
 		}
 		
-		switch (RP.jumpModel) {
+		switch (RAN.jumpModel) {
 			case "off":
 				break;
 				
@@ -81,49 +88,55 @@ var RP = module.exports = {
 		else
 			for (var Pfr = P[fr],u=Math.random(),to=0; to < K && Pfr[to] <= u; to++) ;
 
-		R[fr][fr] = expdev( R[fr][to] );
-		RP.jumps++;
+		RAN.jumps++;
+		cb( to, R[fr][fr] = expdev( R[fr][to] ) );
 
 		return to;
 	},
 	
 	step: function (cb) {
-		var E=RP.E,H=RP.H,R=RP.R,I=RP.I,T=RP.T;
-		var jump=RP.jump;
-		var t = RP.t += RP.dt, steps = RP.steps++;
-		var x = RP.x;
+		var E=RAN.E,H=RAN.H,R=RAN.R,I=RAN.I,T=RAN.T;
+		var jump=RAN.jump;
+		var t = RAN.t += RAN.dt, steps = RAN.steps++;
+		var x = RAN.x, y = RAN.y;
+		var jumpcb = RAN.cb.jump || function () {};
 		
-		cb = cb || function () {};
+		for (var n=0, N=RAN.N; n<N; n++) {  // scan the ensemble
+			if ( t >= H[n] )    // holding time exceeded so jump to new state
+				jump( fr = E[n], function (to, h) {  // get new state
+					E[n] = to;
 
-		for (var n=0, N=RP.N; n<N; n++) {  // scan the ensemble
-			if ( t >= H[n] ) {  // holding time exceeded so jump to new state
-				var fr = E[n], to = E[n] = jump( fr );  // get new state
-				var h = R[fr][fr]; // get exp time drawn
-
-				cb(n,fr,to,h,x); // callback with jump info
+					jumpcb(n,fr,to,h,x); // callback with jump info
 				
-				T[ I[n] ][to] += (t-H[n])+h;  // advance cummulative time-in-state  
-				H[n] = t+h;    // advance next-jump time 
-			}
+					T[ I[n] ][to] += (t-H[n])+h;  // advance cummulative time-in-state  
+					H[n] = t+h;    // advance next-jump time 
+				});
 		}
 
-		RP.gamma = RP.corr();
+		RAN.gamma = RAN.corr();
+		cb(y);
 	},
 		
 	run: function (steps, cb) {	
-		var step = RP.step;
+		var step = RAN.step;
 		
-		while (steps--) step(cb);
-		return RP;
+		while (steps--) step(cb || cb.step);
+
+		if (save = RAN.cb.save) {
+			if (y = RAN.y) save(y,"step obs");
+			if (x = RAN.x) save(x,"jump obs");
+		}
+
+		return RAN;
 	},
 	
 	reset: function () {
-		RP.t = 0;
-		return RP;
+		RAN.t = 0;
+		return RAN;
 	},
 	
 	corr: function () {
-		var K = RP.K, T = RP.T, jumps = RP.jumps, sym = RP.sym, cor = 0;
+		var K = RAN.K, T = RAN.T, sym = RAN.sym, cor = 0;
 
 		for (var fr=0; fr<K; fr++) {
 			for (var Tfr=T[fr], Tsum=0, to=0; to<K; to++) Tsum += Tfr[to];
@@ -137,7 +150,7 @@ var RP = module.exports = {
 	},
 
 	rates: function () {
-		var K = RP.K, T = RP.T, jumps = RP.jumps, sym = RP.sym, cor=0, Tmax=0,max=Math.max;
+		var K = RAN.K, T = RAN.T, jumps = RAN.jumps, sym = RAN.sym, cor=0, Tmax=0,max=Math.max;
 
 		for (var fr=0; fr<K; fr++) for (var Tfr=T[fr],to=0; to<K; to++) 
 			Tmax = max(Tmax, Tfr[to]);
@@ -158,51 +171,51 @@ var RP = module.exports = {
 			return rtn;
 		}
 
-		if (opts) Copy(opts, RP);
+		if (opts) Copy(opts, RAN);
 
-		var N = RP.N;
+		var N = RAN.N;
 
-		if (RP.A) { // K-state markov
+		if (RAN.A) { // K-state markov
 		}
 		else
-		if (RP.p || RP.Tc) {  // two-state markov process via p,Tc parms
+		if (RAN.p || RAN.Tc) {  // two-state markov process via p,Tc parms
 			var
-					p = RP.p, 
-					Tc = RP.Tc, 
-					q = RP.q = 1 - p,
-					alpha = RP.alpha = 2.3 * q / Tc, 
-					beta = RP.beta = 2.3 * p / Tc;
+					p = RAN.p, 
+					Tc = RAN.Tc, 
+					q = RAN.q = 1 - p,
+					alpha = RAN.alpha = 2.3 * q / Tc, 
+					beta = RAN.beta = 2.3 * p / Tc;
 
-			RP.A = [[-alpha, alpha], [beta, -beta]];
-			RP.pi = [p, q];
+			RAN.A = [[-alpha, alpha], [beta, -beta]];
+			RAN.pi = [p, q];
 		}
 		else
-		if (RP.alpha || RP.beta) { // two-state markov process via alpha,beta parms
+		if (RAN.alpha || RAN.beta) { // two-state markov process via alpha,beta parms
 			var 
-				alpha = RP.alpha, 
-				beta = RP.beta, 
-				p = RP.p = alpha / (alpha + beta), 
-				q = RP.q = 1 - p;
+				alpha = RAN.alpha, 
+				beta = RAN.beta, 
+				p = RAN.p = alpha / (alpha + beta), 
+				q = RAN.q = 1 - p;
 
-			RP.A = [[-alpha, alpha], [beta, -beta]];
-			RP.pi = [p, q];			
+			RAN.A = [[-alpha, alpha], [beta, -beta]];
+			RAN.pi = [p, q];			
 		}
 		else
 			console.log("Houston we have a problem");
 		
 		// compute nyquist sampling rate
 
-		var A = RP.A, K = RP.K = A.length;
-		var lambda = RP.lambda = meanValue(A);
-		var Tc = RP.Tc = 2.3 / lambda;
-		var dt = RP.dt = Tc / RP.nyquist;
+		var A = RAN.A, K = RAN.K = A.length;
+		var lambda = RAN.lambda = meanValue(A);
+		var Tc = RAN.Tc = 2.3 / lambda;
+		var dt = RAN.dt = Tc / RAN.nyquist;
 		
 		if (1/dt < 2/Tc)
 			console.log("Woa there cowboy - your sampling rate is subNyquist");
 
 		// default state symbols
 	
-		var sym = RP.sym;
+		var sym = RAN.sym;
 		if ( !sym ) {
 			var sym = matrix(K);
 			for (var k=0; k<K; k++) sym[k] = k;
@@ -210,13 +223,13 @@ var RP = module.exports = {
 
 		// default equilib probabilities
 		
-		var pi = RP.pi;		
+		var pi = RAN.pi;		
 		if ( !pi ) {
-			var pi = RP.pi = matrix(K);
+			var pi = RAN.pi = matrix(K);
 			for (var fr=0,pi0=1/K; fr<K; fr++) pi[fr] = pi0;
 		}
 		
-		var p = RP.p = pi[0], q = RP.q = 1-p;
+		var p = RAN.p = pi[0], q = RAN.q = 1-p;
 
 		// enforce global balance
 		
@@ -229,12 +242,12 @@ var RP = module.exports = {
 
 		// allocate the ensemble
 		
-		var R = RP.R = matrix(K,K);		
-		var P = RP.P = matrix(K,K);
-		var T = RP.T = matrix(K,K);
-		var E = RP.E = matrix(N);
-		var H = RP.H = matrix(N);
-		var I = RP.I = matrix(N);
+		var R = RAN.R = matrix(K,K);		
+		var P = RAN.P = matrix(K,K);
+		var T = RAN.T = matrix(K,K);
+		var E = RAN.E = matrix(N);
+		var H = RAN.H = matrix(N);
+		var I = RAN.I = matrix(N);
 
 		// compute holding times
 
@@ -244,10 +257,10 @@ var RP = module.exports = {
 		
 		// initialize poisson jump model
 
-		var jump=RP.jump;
+		var jump=RAN.jump;
 		
-		for (var fr=0; fr<K; fr++) jump(fr);
-		RP.jumpModel = "off";
+		for (var fr=0; fr<K; fr++) jump(fr, function () {});
+		RAN.jumpModel = "off";
 		
 		// seed the ensemble
 
@@ -269,20 +282,21 @@ var RP = module.exports = {
 			}
 
 		else  // multi-state 
-			for (var n=0; n<N; n++) {
-				var k = I[n] = E[n] = jump( fr = floor(rand() * K) );
-				var h = H[n] = R[fr][fr];	
-				T[k][k] += h;
-			}
+			for (var n=0; n<N; n++) 
+				jump( fr = floor(rand() * K), function (to,h) {
+					var k = I[n] = E[n] = to;
+					H[n] = h;	
+					T[k][k] += h;
+				});
 
 		// initial symbol-value normalized correlation
 
-		RP.corr0=0;
-		for (var k=0; k<K; k++) RP.corr0 += sym[k] * sym[k];
+		RAN.corr0=0;
+		for (var k=0; k<K; k++) RAN.corr0 += sym[k] * sym[k];
 			
-		RP.gamma = RP.corr() / RP.corr0;
+		RAN.gamma = RAN.corr() / RAN.corr0;
 
-		return RP;
+		return RAN;
 	}
 };
 
@@ -298,39 +312,47 @@ function meanValue(A) {
 	return lambda / (K*K-K);
 }
 	
-var 
-	mvp = {mu: [1,2], sigma: [[.9,.6],[.6,.7]]},
-	mvd = MVN(mvp.mu, mvp.sigma);
+function test() {
+	var 
+		mvp = {mu: [1,2], sigma: [[.9,.6],[.6,.7]]},
+		mvd = RAN.MVN(mvp.mu, mvp.sigma);
 
-RP.config({
-	N: 2000,
-	//A: [[0,1,2],[3,0,4],[5,6,0]],
-	//sym: [-1,0,1],
+	RAN.config({
+		N: 2000,
+		//A: [[0,1,2],[3,0,4],[5,6,0]],
+		//sym: [-1,0,1],
 
-	A: [[0,1],[1,0]], //[[0,1,2],[3,0,4],[5,6,0]],
-	sym: [-1,1],
-	nyquist: 10,
-	x: []
-	//p: 0,
-	//Tc: 1
-});
+		A: [[0,1],[1,0]], //[[0,1,2],[3,0,4],[5,6,0]],
+		sym: [-1,1],
+		nyquist: 10,
+		x: [],
+		y: [],
+		cb: {
+			jump: function (n,fr,to,h,x) {
+				//x.push( mvd.sample() );
+				//console.log(["jump",RAN.jumps,RAN.steps,n,fr,to]);
+				//console.log(RAN.R);
+				//console.log([RAN.steps,n,fr,to,RAN.T]);
+				//console.log(["jump",n]);
+			}
+		}
+	});
 
-console.log({
-	jumpRates: RP.A,
-	cumTxPr: RP.P,
-	jumpCounts: RP.T,
-	holdTimes: RP.R,
-	eqPr: RP.pi,
-	Tc: RP.Tc,
-	p: RP.p,
-	dt: RP.dt,
-	cor: RP.gamma
-});
+	console.log({
+		jumpRates: RAN.A,
+		cumTxPr: RAN.P,
+		jumpCounts: RAN.T,
+		holdTimes: RAN.R,
+		eqPr: RAN.pi,
+		Tc: RAN.Tc,
+		p: RAN.p,
+		dt: RAN.dt,
+		cor: RAN.gamma
+	});
 
-RP.run(20, function (n,fr,to,h,x) {
-	//x.push( mvd.sample() );
-	//console.log(["jump",RP.jumps,RP.steps,n,fr,to]);
-	//console.log(RP.R);
-	//console.log([RP.steps,n,fr,to,RP.T]);
-	console.log( [RP.t,RP.gamma, Math.exp(-RP.t/RP.Tc)] );
-});
+	RAN.run(20, function (y) {
+		var n = RAN.t / RAN.Tc;
+		// y.push( [n, RAN.gamma, Math.exp(-n)] );
+		console.log( [n, RAN.gamma, Math.exp(-n)] );
+	});
+}
